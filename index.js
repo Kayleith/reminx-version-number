38751caa8af2a777cca55e437c6d799a3b5c9268
Brian Kang <brian.john.kang@gmail.com>
2018-12-10 14:50:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,7 @@ import { NativeModules } from 'react-native';
 
 const { RNVersionNumber } = NativeModules;
 
-type VersionObject = {
-  appVersion: ?string,
-  buildVersion: ?string,
-  bundleIdentifier: ?string,
-  basestationAppVersion: ?string,
-  basestationBuildVersion: ?string,
-  basestationBundleIdentifier: ?string,
-  getVersionName: ?function
-};
-
-const VersionNumber: VersionObject = {
+const VersionNumber = {
   appVersion: RNVersionNumber && RNVersionNumber.appVersion,
   buildVersion: RNVersionNumber && RNVersionNumber.buildVersion,
   bundleIdentifier: RNVersionNumber && RNVersionNumber.bundleIdentifier,
