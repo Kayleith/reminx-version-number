cdb3acb1d72aea7f846ef50ec30b0a0f316d5652
Brian Kang <brian.john.kang@gmail.com>
2018-12-10 10:45:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ const { RNVersionNumber } = NativeModules;
 type VersionObject = {
   appVersion: ?string,
   buildVersion: ?string,
-  bundleIdentifier: ?string
+  bundleIdentifier: ?string,
+  basestationAppVersion: ?string,
+  basestationBuildVersion: ?string,
+  basestationBundleIdentifier: ?string
 };
 
 const VersionNumber: VersionObject = {
   appVersion: RNVersionNumber && RNVersionNumber.appVersion,
   buildVersion: RNVersionNumber && RNVersionNumber.buildVersion,
-  bundleIdentifier: RNVersionNumber && RNVersionNumber.bundleIdentifier
+  bundleIdentifier: RNVersionNumber && RNVersionNumber.bundleIdentifier,
+  basestationAppVersion: RNVersionNumber && RNVersionNumber.basestationAppVersion,
+  basestationBuildVersion: RNVersionNumber && RNVersionNumber.basestationBuildVersion,
+  basestationBundleIdentifier: RNVersionNumber && RNVersionNumber.basestationBundleIdentifier
 };
 
 export default VersionNumber;
